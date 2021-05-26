1dc7cf3a763707751d5bf6e9a53d964a48c1bba4
rehmatkarim <p146120@gmail.com>
2021-05-26 14:20:05 -0400

    lil changing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import { Admin, Resource  } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
-import { UserList } from './components/users';
-import { PostList,PostEdit,PostCreate } from './components/Posts';
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import { UserList } from "./components/users";
+import { PostList, PostEdit, PostCreate } from "./components/Posts";
 import Dashboard from "./components/Dashboard";
 import authProvider from "./auth/authProvider";
 import NotFound from "./components/NotFound";
-import { createMuiTheme } from '@material-ui/core/styles';
-import PostIcon from '@material-ui/icons/Book';
-import UserIcon from '@material-ui/icons/Group';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+import { createMuiTheme } from "@material-ui/core/styles";
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const theme = createMuiTheme({
   palette: {
     primary: {
       main: "#1976d2",
     },
     secondary: {
-      main:"#388e3c",
+      main: "#388e3c",
     },
   },
 });
 function App() {
   return (
     <div className="App">
-      <Admin theme={theme} catchAll={NotFound} dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
-      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
-        <Resource name="users" list={UserList} icon={UserIcon}/>
+      <Admin
+        theme={theme}
+        catchAll={NotFound}
+        dashboard={Dashboard}
+        authProvider={authProvider}
+        dataProvider={dataProvider}
+      >
+        <Resource
+          name="posts"
+          list={PostList}
+          edit={PostEdit}
+          create={PostCreate}
+          icon={PostIcon}
+        />
+        <Resource name="users" list={UserList} icon={UserIcon} />
       </Admin>
     </div>
   );
