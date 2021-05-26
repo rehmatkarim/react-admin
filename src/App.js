74e650bb36df3b8dafc95e1fa6eee827980545c2
rehmatkarim <p146120@gmail.com>
2021-05-26 12:38:54 -0400

    dashboard login and fake auth added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Admin, Resource  } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './components/users';
 import { PostList,PostEdit,PostCreate } from './components/Posts';
+import Dashboard from "./components/Dashboard";
+import authProvider from "./auth/authProvider";
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
@@ -9,7 +11,7 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 function App() {
   return (
     <div className="App">
-      <Admin dataProvider={dataProvider}>
+      <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
         <Resource name="users" list={UserList} icon={UserIcon}/>
       </Admin>
