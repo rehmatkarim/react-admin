621cf5b695692aa378e74d97c10e67fe288a2f93
rehmatkarim <p146120@gmail.com>
2021-05-26 12:48:47 -0400

    added mobile responsivness of posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,7 +1,9 @@
 import * as React from "react";
+import { useMediaQuery } from '@material-ui/core';
 import {
   Filter,
   List,
+  SimpleList,
   Datagrid,
   TextField,
   ReferenceField,
@@ -39,20 +41,30 @@ const PostFilter = (props) => (
       </ReferenceInput>
   </Filter>
 );
-export const PostList = (props) => (
-  <List filters={<PostFilter />} {...props}>
-    <Datagrid>
-      <TextField source="id" />
-      <ReferenceField source="userId" reference="users">
-        <TextField source="name" />
-      </ReferenceField>
-
-      <TextField source="title" />
-
-      <EditButton />
-    </Datagrid>
-  </List>
-);
+export const PostList = (props) => {
+  const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+  return (
+      <List {...props}>
+          {isSmall ? (
+              <SimpleList
+                  primaryText={record => record.title}
+                  secondaryText={record => `${record.views} views`}
+                  tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+              />
+          ) : (
+              <Datagrid>
+                  <TextField source="id" />
+                  <ReferenceField label="User" source="userId" reference="users">
+                      <TextField source="name" />
+                  </ReferenceField>
+                  <TextField source="title" />
+                  <TextField source="body" />
+                  <EditButton />
+              </Datagrid>
+          )}
+      </List>
+  );
+}
 export const PostCreate = (props) => (
   <Create {...props}>
     <SimpleForm>
