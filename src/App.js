a5790702ee7fa11b240c4c8ab4f767375d522d59
rehmatkarim <p146120@gmail.com>
2021-05-26 12:28:05 -0400

    sidebar icons changed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { Admin, Resource  } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './components/users';
 import { PostList,PostEdit,PostCreate } from './components/Posts';
-
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 function App() {
   return (
     <div className="App">
       <Admin dataProvider={dataProvider}>
-      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-        <Resource name="users" list={UserList}/>
+      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon}/>
       </Admin>
     </div>
   );
