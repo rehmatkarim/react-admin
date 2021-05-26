55b9572122e669d32932e253416e340f05b4642f
rehmatkarim <p146120@gmail.com>
2021-05-26 12:15:57 -0400

    crud opertations added and search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Admin, Resource  } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './components/users';
-import { PostList,PostEdit } from './components/Posts';
+import { PostList,PostEdit,PostCreate } from './components/Posts';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Admin dataProvider={dataProvider}>
-      <Resource name="posts" list={PostList} edit={PostEdit} />
+      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
         <Resource name="users" list={UserList}/>
       </Admin>
     </div>
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,19 +1,25 @@
 import * as React from "react";
 import {
+  Filter,
   List,
   Datagrid,
   TextField,
   ReferenceField,
   EditButton,
   Edit,
+  Create,
   SimpleForm,
   ReferenceInput,
   SelectInput,
   TextInput,
 } from "react-admin";
 
+const PostTitle = ({ record }) => {
+      return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
 export const PostEdit = (props) => (
-  <Edit {...props}>
+  <Edit title={<PostTitle />} {...props}>
     <SimpleForm>
       <TextInput disabled source="id" />
       <ReferenceInput source="userId" reference="users">
@@ -25,8 +31,16 @@ export const PostEdit = (props) => (
     </SimpleForm>
   </Edit>
 );
+const PostFilter = (props) => (
+  <Filter {...props}>
+      <TextInput label="Search" source="q" alwaysOn />
+      <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+          <SelectInput optionText="name" />
+      </ReferenceInput>
+  </Filter>
+);
 export const PostList = (props) => (
-  <List {...props}>
+  <List filters={<PostFilter />} {...props}>
     <Datagrid>
       <TextField source="id" />
       <ReferenceField source="userId" reference="users">
@@ -38,4 +52,16 @@ export const PostList = (props) => (
       <EditButton />
     </Datagrid>
   </List>
+);
+export const PostCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
+  </Create>
 );
